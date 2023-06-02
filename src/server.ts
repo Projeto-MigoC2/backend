f31e9d026086b61d043424f7bad359a6ef2090f4
Leo <leleolagomoreno@gmail.com>
2023-06-01 23:04:10 -0300

    fix: novo metodo de tratamento de erro

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 import "./shared/container";
+import { AppError } from "./errors/AppError";
 
 // AppDataSource.initialize()
 //   .then(() => {
@@ -26,6 +27,14 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
+
+
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
     return response.status(500).json({
       status: "Error",
       message: `Internal server error ${err.message}`,
